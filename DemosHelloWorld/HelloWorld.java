d348d09a5b725084f72537d35637617e37ac15bc
TunnRKA <96030186+TunnRKA@users.noreply.github.com>
2022-03-09 18:26:40 +0100

    testing for syntax

--- DemosHelloWorld/HelloWorld.java
+++ DemosHelloWorld/HelloWorld.java
@@ -3,5 +3,8 @@
 class HelloWorld {
     public static void main ( String[] args ) {
         System.out.println ( "Hello, World!" );
+        for( int i = 0 ; i < 3 ; i++ ) {
+          System.out.println ( "I'm number " + i );
+        }
     }
 }
