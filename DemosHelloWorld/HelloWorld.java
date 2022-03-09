342640ed91d4190bf5a9c2cb408a19fea8f58247
TunnRKA <96030186+TunnRKA@users.noreply.github.com>
2022-03-09 16:48:38 +0100

    testing static

--- DemosHelloWorld/HelloWorld.java
+++ DemosHelloWorld/HelloWorld.java
@@ -1,7 +1,7 @@
 // My First Java Program
 
 class HelloWorld {
-    public static void main ( String [] args ) {
+    public static void main ( String[] args ) {
         System.out.println ( "Hello, World!" );
     }
 }
