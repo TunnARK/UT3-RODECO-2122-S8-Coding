955ab3672f188023e2e22bc48abe8ab290d22ab3
TunnRKA <96030186+TunnRKA@users.noreply.github.com>
2022-04-07 16:40:56 +0200

    Upload Seance 2 End - Code MaL - TMOSED TP3

--- 2122-S8-TMOSED-TP-03/Seance2/CalculDesSorties.c
+++ 2122-S8-TMOSED-TP-03/Seance2/CalculDesSorties.c
@@ -1,4 +1,1 @@
 //Declaration des places  et marquage initial
-Moteur = p1 || p4;
-Alarme = p3;
-Sens = p4;
--- 2122-S8-TMOSED-TP-03/Seance2/LesPlaces.c
+++ 2122-S8-TMOSED-TP-03/Seance2/LesPlaces.c
@@ -1,5 +1,1 @@
 //Declaration des places  et marquage initial
-int p1=1 ; 
-int p3=0 ; 
-int p2=1 ; 
-int p4=0 ; 
