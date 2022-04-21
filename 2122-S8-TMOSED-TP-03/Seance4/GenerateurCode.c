7a216322fc70ed4783983002d57ff2af26871059
TunnRKA <96030186+TunnRKA@users.noreply.github.com>
2022-04-21 16:25:12 +0200

    Changing method (Predicat's negation) - Code MaL - TP3 TMOSED

--- 2122-S8-TMOSED-TP-03/Seance4/GenerateurCode.c
+++ 2122-S8-TMOSED-TP-03/Seance4/GenerateurCode.c
@@ -21,8 +21,8 @@ void Negation ( Transition * T, char * NegPred ) {
 
 		T->Predicat[get_index(T->Predicat, x)+1] = '0' ; // remplace 1 par 0
 
-		NegPred = T->Predicat ; // stocke la negationd du predicat dans NegPred
-
+		*       NegPred = T->Predicat ; // stocke la negation du predicat dans NegPred
+		printf("\n\n->%s\n\n", NegPred );
 	} else {
 
 		if ( T->Predicat[get_index(T->Predicat, x)+1] == zero[0] ) {
@@ -30,7 +30,7 @@ void Negation ( Transition * T, char * NegPred ) {
 			T->Predicat[get_index(T->Predicat, x)+1] = '1' ; // remplace 0 par 1
 
 			NegPred = T->Predicat ; // stocke la negationd du predicat dans NegPred
-
+			printf("\n\n-->%s\n\n", NegPred );
 		} else { printf("\n\n \t !!! \t error: \t no ' found in predicat\n\n"); }
 	}
 }
@@ -81,34 +81,35 @@ void GenererBlocF (Transition * Tstart, char LeFichierEnC[MAX_NOM]){
 
 		// Ecriture autorisee
 		if ( flag == 1 ) {
-			printf("coucou\n");
+			//printf("\ncoucou1\n");
 			// Ecriture de l equation
 			printf("\t ES_%c = EP_%c and %s", Tencours->ArcsSortants->Place[1], Tencours->ArcsEntrants->Place[1], Tencours->Predicat );
 
+			//printf("\ncoucou2\n");
 			Trace[indice] = Tencours->ArcsSortants->Place[1] ; // Tracabilite des places deja traitees
 
 			Tinter = Tencours->Suivant ; // Tinter prends la valeur de Tencours Suivante
 
+			//printf("coucou3\n");
+
 			while ( Tinter != NULL ) {
 				// Si Tinter et Tencours ont d autres places sortants en commun alors completer equation
+				//printf("coucou4\n");
 				if ( Tinter->ArcsSortants->Place[1] == Tencours->ArcsSortants->Place[1] ) {
 					printf(" or EP_%c and %s", Tinter->ArcsEntrants->Place[1], Tinter->Predicat );
+					//printf("coucou5\n");
 				}
 				Tinter = Tinter->Suivant ; // Tinter Suivante
 			}
 
-			Tinter2 = Tencours->Suivant ; // Tinter2 prends la valeur de Tencours Suivante
+			Tinter2 = Tencours ; // Tinter2 prends la valeur de Tencours Suivante
 
 			while ( Tinter2 != NULL ) {
 				// Regrouper les termes de maintiens (chercher les transitions non sensibilisees de place en cours)
-				if ( Tinter->ArcsSortants->Place[1] == Tencours->ArcsSortants->Place[1] ) {
-					char * NegPred = "" ;
-
-					printf("\n\n%s\n\n", NegPred );
-
-					Negation( Tinter2, NegPred );
-					printf("\n\n%s\n\n", NegPred );
-					printf(" or EP_%c and %s", Tinter->ArcsSortants->Place[1], NegPred );
+				if ( Tinter2->ArcsSortants->Place[1] == Tencours->ArcsSortants->Place[1] ) {
+					char * NegPred ;
+					Negation( Tinter2, &NegPred );
+					printf(" or EP_%c and %s", Tinter2->ArcsSortants->Place[1], NegPred );
 				}
 				Tinter2 = Tinter2->Suivant ; // Tinter2 Suivante
 			}
